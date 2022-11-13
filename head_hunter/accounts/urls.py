c0afd7ff848464458d69238b62ca3d6e6497af43
Mosunov Anton <antonmossunov@gmail.com>
2022-11-13 15:21:58 +0600

    Реализация профиля пользователя.
    Созданы MVC для функционала детальной страницы профиля, редактирования сведений о поьзователе и изменения пароля. Созданы шаблоны, настроена маршрутизация, реализованы представления.

--- head_hunter/accounts/urls.py
+++ head_hunter/accounts/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from accounts.views import LoginView, logout_view, RegisterView
-# from accounts.views import UserPasswordChangeView
+from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView, UserPasswordChangeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    # path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
-    # path('profile/<int:pk>/change', UserChangeView.as_view(), name='change'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/change', UserChangeView.as_view(), name='change'),
     path('logout/', logout_view, name='logout'),
-    # path('profile/<int:pk>/change', UserChangeView.as_view(), name='change'),
-    # path('<int:pk>/password_change', UserPasswordChangeView.as_view(), name='password_change')
+    path('<int:pk>/password_change', UserPasswordChangeView.as_view(), name='password_change')
 ]
--- head_hunter/accounts/views.py
+++ head_hunter/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.db.models import Q
@@ -8,7 +9,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from accounts.forms import LoginForm
 from django.core.paginator import Paginator
 
-from accounts.forms import CustomUserCreationForm
+from accounts.forms import CustomUserCreationForm, UserChangeForm, PasswordChangeForm
+
+from accounts.models import Account
+
 
 # from accounts.forms import UserChangeForm
 # from posts.models import Post
@@ -66,3 +70,63 @@ class RegisterView(CreateView):
         context = {}
         context['form'] = form
         return self.render_to_response(context)
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # reviews = Review.objects.filter(is_deleted=False, author=self.request.user)
+        # context['reviews'] = reviews
+        return context
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data()
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+        return UserChangeForm(**form_kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        response = super(UserChangeView, self).form_valid(form)
+        profile_form.save()
+        return response
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('login')
